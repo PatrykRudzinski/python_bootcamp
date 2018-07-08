e7f24c828d046ba9873c45f07c2d04d82d193d4e
alan <rudzinski.a@gmail.com>
2018-07-08 14:30:32 +0200

    zadanie 3

---
+++ jakies tam notatki/funkcje/zadania funkcje/zadanie 3.py
@@ -0,0 +1,28 @@
+def liczba_znaków_pomiędzy(napis, znak_str='<', znak_stp='>'):
+    suma = 0
+    start = 0
+    for i in napis:
+        if i == znak_str:
+            start += 1
+        elif i == znak_stp:
+            start -= 1
+        elif start:
+            suma += start
+    return suma
+
+
+
+
+
+
+
+
+
+def test_liczba_znaków_pomiędzy_znaki_podstawowe():
+     assert liczba_znaków_pomiędzy('ala ma <kota> a kot ma ale') == 4
+
+def test_liczba_znaków_pomiędzy_znaki_inne():
+    assert liczba_znaków_pomiędzy('ala [kota [a kot]] ma [ale]', '[', ']') == 18
+
+def test_liczba_znaków_pomiędzy_znaki_podstawowe_zagniezdzone():
+    assert liczba_znaków_pomiędzy('a <a<a<a>>>') == 6
