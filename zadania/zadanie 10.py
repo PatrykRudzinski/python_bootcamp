eaaf9558763ff6c7db6b72d4ec8d99c803f09025
alan <rudzinski.a@gmail.com>
2018-07-08 09:19:45 +0200

    Zadanie 10 poprawki

--- zadania/zadanie 10.py
+++ zadania/zadanie 10.py
@@ -1,5 +1,6 @@
-text = input('Podaj napis: ')
+text = input('Podaj napis: ').lower()
 ilosci = {}
 for l in text:
     ilosci[l] = ilosci.get(l, 0) + 1
-print(ilosci)
+for k, v in ilosci.items():
+    print(f'Znak {k} wystąpił {v} razy.')
